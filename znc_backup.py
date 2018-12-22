b0ed7a78c779e6478bddfa1d5e1b54d1dbbad960
Matthew Jorgensen <matthew@jrgnsn.net>
2018-12-22 16:21:05 -0600

    Replaced `print()` statements with `logger.info()`
    Increased version from 0.1.0 to 0.2.0.

--- znc_backup.py
+++ znc_backup.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 # znc-backup
-# version: 0.1.0
+# version: 0.2
 # This script compresses your znc data dir and emails it to you once a week.
-
 import os
 import json
 import logging
@@ -19,43 +18,34 @@ import conf.logger_config as lc
 
 def startLogger():
     if not os.path.isdir('log'):
-        subprocess.call(['mkdir','log'])
-
+        subprocess.call(['mkdir', 'log'])
     logging.config.dictConfig(lc.LOGGER_CONFIG)
-
     logger = logging.getLogger(__name__)
     logger.debug('Logger initialized.')
-
     return logger
 
 
 def sendEmail(backupFile):
-
     host = config['smtp']['host']
     port = config['smtp']['port']
     username = config['smtp']['username']
     password = config['smtp']['password']
-
     toAddr = config['email']['to']
     fromAddr = config['email']['from']
-
     message = """
 It's that time of week again.
 Here's your weekly backup of your znc data on `Chell`.
     """
-
     msg = EmailMessage()
     msg.set_content(message)
     msg['Subject'] = 'Weekly ZNC Backup'
     msg['From'] = fromAddr
     msg['To'] = toAddr
-
     with open(backupFile, 'rb') as bf:
         msg.add_attachment(
             bf.read(), maintype="application",
             subtype="x-7z-compressed", filename=os.path.basename(backupFile)
             )
-
     try:
         logger.debug('Attempting to send mail...')
         with smtplib.SMTP_SSL(host, port) as s:
@@ -65,25 +55,20 @@ Here's your weekly backup of your znc data on `Chell`.
     except Exception as e:
         logger.critical('Unable to send mail. Exception: {}'.format(e))
 
-def main():
 
+def main():
     global logger
     logger = startLogger()
-
     global config
     if not os.path.exists('config.json'):
         logger.warning('Configuration file doesn\'t exist.')
         config = setup.createConfig()
-
     with open('config.json', 'r') as f:
         config = json.load(f)
-
     path = os.getenv("HOME") + "/.znc"
     tempPath = os.getenv("HOME") + "/znc-backup-staging"
     timestamp = datetime.now().strftime("%d-%h-%y - %H-%M-%S")
-
     logger.debug('Checking if backup source location exists.')
-
     if not os.path.isdir(tempPath):
         logger.warning('Backup dir doesn\'t exist. Creating...')
         try:
@@ -96,26 +81,19 @@ def main():
     else:
         logger.debug('Backup dir exists - skipping')
         # createdBackupDir = False
-
     filename = "znc - " + timestamp
     outFile = tempPath + "/" + filename + ".7z"
-    print(outFile)
-
+    logger.info(outFile)
     if not os.path.isdir(path):
         logger.error('Uh oh... check to make sure the path is right.')
         logger.error('`path`: {}'.format(path))
     else:
-        cmd = []
-        cmd.append("7z")
-        cmd.append("a")
-        cmd.append(outFile)
-        cmd.append(path)
+        cmd = ["7z", "a", outFile, path]
         try:
             subprocess.call(cmd)
         except Exception:
-            logger.error('Something went wrong attemping to send email.')
+            logger.error('Something went wrong creating archive.')
             exit()
-
         sendEmail(outFile)
 
 
